3fc6e31300d5ad97d548c2c9286ed320267de97a
Huy Chau <huy.chau@asnet.com.vn>
2019-04-08 14:16:40 +0700

    Fix user password not hash

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Friend
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Friend)
