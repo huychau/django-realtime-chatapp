61730ba5886e938f8967df7a3d580d34250784cd
Huy Chau <huy.chau@asnet.com.vn>
2019-04-09 10:41:49 +0700

    Implement to add creator as request user by default when creating new room

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Room, Participant, Message
 
-# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Participant)
+admin.site.register(Message)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.db import IntegrityError
+from rest_framework import serializers
+from auth.permissions import (
+    IsAdminOrIsSelf,
+    IsSelfOrAdminUpdateDeleteOnly,
+)
+from .serializers import (
+    RoomSerializer,
+    MessageSerializer,
+    ParticipantSerializer,
+)
+from .models import Room, Participant, Message
 
-# Create your views here.
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = (IsSelfOrAdminUpdateDeleteOnly,
+                          IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        """
+        Add `creator` param as request user when creating new room
+        """
+        serializer.save(creator=self.request.user)
+
