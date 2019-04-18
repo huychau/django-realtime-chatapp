7526a20c7a34deb294378881fa850ea11fca3812
Huy Chau <huy.chau@asnet.com.vn>
2019-04-18 15:07:08 +0700

    Remove some permissions unuse

--- auth/permissions.py
+++ auth/permissions.py
@@ -1,24 +1,6 @@
 from rest_framework import permissions
 
 
-class IsSelf(permissions.BasePermission):
-    """
-    Check user request the resources he/she is owner
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
-
-
-class IsAdmin(permissions.BasePermission):
-    """
-    Check user request is admin
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user.is_superuser
-
-
 class IsAdminOrIsSelf(permissions.BasePermission):
     """
     Check user request is admin or self
@@ -26,36 +8,6 @@ class IsAdminOrIsSelf(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj == request.user or request.user.is_superuser
-
-
-class IsSelfOrReadOnly(permissions.BasePermission):
-    """
-    Check the owner can read only
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.id == request.user.id
-
-
-class IsReadOnly(permissions.BasePermission):
-    """
-    Is read only permission
-    """
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-
-class IsAdminListOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow access to lists for admins
-    """
-
-    def has_permission(self, request, view):
-        return view.action != 'list' or request.user \
-            and request.user.is_superuser
 
 
 class IsAdminReadOnly(permissions.BasePermission):
