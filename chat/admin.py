db60fa5b00882cd85638daf4eb61cdf746865267
Huy Chau <huy.chau@asnet.com.vn>
2019-04-23 15:37:43 +0700

    Update Profile, Friend, Room, Message admin sites

--- chat/admin.py
+++ chat/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Room, Message
 
 
-admin.site.register(Room)
-admin.site.register(Message)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'label', 'user', 'users')
+    search_fields = ('name', 'user__username')
+    list_filter = ('user',)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'message', 'user', 'room')
+    search_fields = ('message', 'subject', 'user__username')
+    list_filter = ('user', 'room',)
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
--- user/admin.py
+++ user/admin.py
@@ -3,6 +3,17 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Friend
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar', 'phone', 'bio', 'address')
+    search_fields = ('phone', 'user__username')
+    list_filter = ('user',)
+
+
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'message')
+    search_fields = ('from_user', 'to_user')
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile)
-admin.site.register(Friend)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Friend, FriendAdmin)
